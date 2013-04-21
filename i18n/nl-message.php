e3138de0b3daa528b68b38dd91738cd617c2fe64
djgprv <djgprv@gmail.com>
2013-04-21 21:30:19 +0200

    Dutch translate by fortron, fixed xhtml error by webklaus.

---
+++ i18n/nl-message.php
@@ -0,0 +1,44 @@
+<?php
+    return array(
+		'[djg] Contact form' =>'[djg] Contact formulier',
+		'Ajax contact form' =>'Ajax contact formulier',
+		//Fronted
+    	'Contact Us' => 'Neem contact met ons op',
+		'Your Name:' => 'Je Naam:',
+		'Your Email:' => 'Je E-mailadres:',
+		'The Subject:' => 'Je Onderwerp:',
+		'The Message:' => 'Je Bericht:',
+		'The Captcha:' => 'Captcha:', // 1.1.2
+		'captcha image' => 'captcha afbeelding', // 1.1.2a
+		'Send The Message' => 'Verstuur het bericht',
+		'Select a subject' => 'Selecteer een onderwerp',  // 1.1.2
+		'Choose the topic' => 'Kies een onderwerp',
+		'I don\'t talk to strangers.' => 'Ik praat niet met vreemden.',
+		'You don\'t want me to answer?' => 'Wil je mij niet beantwoorden?',
+		'What is the purpose of the contact?' => 'Wat is het doel van het contact?',
+		'Issue required.' => 'Verplicht.',
+		'Probability JavaScript is disabled or You have not jQuery library.' => 'JavaScript is waarschijnlijk uitgeschakeld of jQuery is niet beschikbaar.',
+		'Forgot why you came here?' => 'Ben je vergeten waarom je hier bent?',
+		'Thank you. The mailman is on his way.' => 'Dankjewel. De e-mail is onderweg.',
+		'Sorry, don\'t know what happened. Try later.' => 'Sorry ik weet niet wat er mis gaat. Probeer het later nog eens.',
+		//Backend
+		'Documentation' => 'Documentatie',
+		'Djg Contact Form Documentation' => 'Djg Contact Formulier Documentatie',
+		'Djg Contact Form Settings' => 'Djg Contact Formulier Instellingen',
+		'E-mail adress' => 'E-mailadres',
+		'Your e-mail adress' => 'Jou e-mailadres',
+		'Subject prefix' => 'Onderwerp voorvoegsel',
+		'Subject prefix <br />for example: <strong>"Message frow www, topic: "</strong>' => 'Onderwerp voorvoegsel<br /> voorbeeld: <strong>"Bericht van www, onderwerp:"</strong>',
+		'Auto append css' => 'CSS automatisch toevoegen', //1.1.2
+		'Choose yes if you want to automatically append <br />example css file to <strong>HEAD</strong> after init.<br /><code>:fl</code>' => 'Kies ja als je automatisch CSS wilt toevoegen.<br />voorbeeld van css bestand na toevoeging aan <strong>HEAD</strong>.<br /><code>:fl</code>', // 1.1.2
+		'Choose yes if you want to use a captcha to protect yourself against spammers.' => 'Kies ja om een captcha tegen span te gebruiken.',
+		'Use captcha' => 'Gebruik captcha',
+		'Yes' => 'Ja',
+		'No' => 'Nee',
+		'Use subject' => 'Gebruik onderwerp', // 1.1.2
+		'Choose yes if you want to use a subjects in your form.' => 'Kies ja als je een onderwerp in het formulier wilt gebruiken.', // 1.1.2
+		'Subjects:' => 'Onderwerpen:', // 1.1.2
+		'List of all avalible subjects of message. <br />Blanks inputs will be skipped.' => 'Lijst van beschikbare onderwerpen. <br />Lege invoervelden worden overgeslagen.',
+		//Massage
+		'Massage from:' => 'Bericht van:',
+    );
